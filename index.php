65f97de65fd6d56265162d2e7c41eeb9bd335598
kendriyadigitalmani <kendriyadigital@gmail.com>
2026-02-22 17:22:51 +0530

    Auto-sync Admin - DESKTOP-FCDK61M - 22-02-2026 17:22:50

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 $ADMIN_USER     = 'admin';
 $ADMIN_PASS     = 'test1234';      // ← CHANGE THIS in real projects!
 $ADMIN_TITLE    = 'Luna Admin Test';
-$VERSION        = '1.2.0';         // ← You can update this manually when you make changes
+$VERSION        = '1.3.0';         // ← You can update this manually when you make changes
 
 // Start session
 session_start();
